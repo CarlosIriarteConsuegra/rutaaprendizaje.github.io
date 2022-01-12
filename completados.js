e632f12201343d1edd3d613aec1dec5e2fa619c7
Carlos Arturo Iriarte Consuegra <77239251+CarlosIriarteConsuegra@users.noreply.github.com>
2022-01-12 10:48:01 -0600

    Update completados.js

--- completados.js
+++ completados.js
@@ -1,27 +1,87 @@
-var completados = document.querySelector("#completados");
-var ol = completados.insertAdjacentElement(
-  "beforeend",
-  document.createElement("ol")
-);
-var li = ol.insertAdjacentElement(
-  "beforeend",
-  document.createElement("li")
-);
-var del = li.insertAdjacentElement(
-  "beforeend",
-  document.createElement("del")
-);
-var a = li.insertAdjacentElement(
-  "beforeend",
-  document.createElement("a")
-);
-a.src = z.src;
-a.target = "_BLANK";
-a.textContent = z.nombre + "   ";
-cursosinlink(z.src,a);
-var a = del.insertAdjacentElement(
-  "beforeend",
-  document.createElement("span")
-);
-span.className = z.clase;
-span.textContent = z.escuela;
+function cursosinlink(link,contenedor){
+  if (link == ""){
+    var span = contenedor.insertAdjacentElement(
+      "beforeend",
+      document.createElement("span")
+    );
+    span.id = "sinlink";
+    span.textContent = "!SIN LINK¡";
+  }
+}
+
+function btnarriba(contenedor){
+    var a = contenedor.insertAdjacentElement(
+      "beforeend",
+      document.createElement("a")
+    );
+    a.className = "arriba";
+    a.href = "#nav"
+    a.textContent = "Volver Arriba";
+}
+
+function info() {
+  jQuery.get("https://carlosiriarteconsuegra.github.io/rutaaprendizaje.github.io/cursos.json", function (data) {
+    $.each(data, function (key, value) {
+      if (key != "1" && key != "2") {
+        var main = document.querySelector("#main");
+        var nav = document.querySelector("#nav");
+
+        var a = document.createElement("a");
+        a.href = "#" + key;
+        a.textContent = value.nombre;
+        li = nav.insertAdjacentElement(
+          "beforeend",
+          document.createElement("li")
+        );
+        li.insertAdjacentElement("beforeend", a);
+
+        var section = document.createElement("section");
+        section.id = key;
+        main.insertAdjacentElement("beforeend", section);
+        var h2 = document.createElement("h2");
+        h2.textContent = value.nombre;
+        section.insertAdjacentElement("beforeend", h2);
+        btnarriba(section);
+        var ol = section.insertAdjacentElement(
+          "beforeend",
+          document.createElement("ol")
+        );
+
+        for (z of value.cursos) {
+          if (z.realizado == "realizando") {
+            realizandocurso();
+          }
+          var completados = document.querySelector("#completados");
+          var ol = completados.insertAdjacentElement(
+            "beforeend",
+            document.createElement("ol")
+          );
+          var li = ol.insertAdjacentElement(
+            "beforeend",
+            document.createElement("li")
+          );
+          var del = li.insertAdjacentElement(
+            "beforeend",
+            document.createElement("del")
+          );
+          var a = li.insertAdjacentElement(
+            "beforeend",
+            document.createElement("a")
+          );
+          a.src = z.src;
+          a.target = "_BLANK";
+          a.textContent = z.nombre + "   ";
+          cursosinlink(z.src,a);
+          var a = del.insertAdjacentElement(
+            "beforeend",
+            document.createElement("span")
+          );
+          span.className = z.clase;
+          span.textContent = z.escuela;
+        }
+      }
+    });
+  });
+}
+
+info();
